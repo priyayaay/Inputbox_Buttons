8fa85ccbc29c8922cd81ab04da3475a3b09ba2a6
priyayaay <priya.jain@rogers.com>
2024-07-13 12:40:27 -0300

    added some buttons and inputs

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn1=document.querySelector(".btn1")
+let input1=document.querySelector(".input1")
+
+function chocolate() {
+
+    console.log(input1.value)
+}
+
+btn1.addEventListener("click", chocolate);
